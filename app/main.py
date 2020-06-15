9c878e2b193df6117ce610a1a0f4bf3299acd608
Krzysztof Ruczkowski <krzysztof.ruczkowski@nokia.com>
2020-06-15 21:43:07 +0200

    program updated

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-print("Hello")
+from time import sleep
+from gpiozero import LED
+
+LED_PIN = 17
+
+led = LED(LED_PIN)
+
+i = 0
+while True:
+    print(f'{i} - Blink!')
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(5)
+    i += 1
